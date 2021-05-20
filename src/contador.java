fe18ed80088122826d6e4ad6a1eecaa638552c29
LillyanaMarreroCaparroso <78912322+LillyanaMarreroCaparroso@users.noreply.github.com>
2021-05-20 11:58:58 +0100

    probando constructor de la clase, falla

---
+++ src/contador.java
@@ -0,0 +1,47 @@
+public class contador {
+
+    /*private static int valorActual = 0 ;
+    private static int incremento = 1;
+    private static int valorLimite;
+
+    public contador(int valorActual, int incremento, int valorLimite) {
+        this.valorActual = valorActual;
+        this.incremento = incremento;
+        this.valorLimite = valorLimite;
+    }
+
+    public int getValorLimite() {
+        return valorLimite;
+    }
+    public void setValorLimite(int valorLimite) {
+        this.valorLimite = valorLimite;
+    }
+    public int getValorActual() {
+        return valorActual;
+    }
+    public void setValorActual(int valorActual) {
+        this.valorActual = valorActual;
+    }
+    public int getIncremento() {
+        return incremento;
+    }
+    public void setIncremento(int incremento) {
+        this.incremento = incremento;
+    }
+
+    public int getValorActual(){
+        return this.valorActual;
+    }
+
+    public void incrementar (){
+       
+    }
+
+    public int contadorInicial(){
+
+    }
+
+    public void limitar(int limite){
+        
+    }*/
+}
---
+++ src/testContador.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+public class testContador {
+
+    //Al incrementar el contador se suma al
+    //valor actual el incremento y nos indican si se superó el límite.
+
+        @Test
+        public void testConstructor(){
+            contador con = new contador(0, 1);
+        }
+    }
